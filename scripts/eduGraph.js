07c02aad8cabe183b1fc64c75636e334ecfaab4d
Navindu <navanavi3@gmail.com>
2022-07-05 17:57:28 +0530

    fix: opacity

--- scripts/eduGraph.js
+++ scripts/eduGraph.js
@@ -2,19 +2,15 @@
     let eplayer = document.getElementById("eplayer_iframe");
 
     if (eplayer) {
-        console.log("Analytics connected");
-
         let video = eplayer.contentWindow.document.getElementsByTagName("video")[0];
 
         video.addEventListener("play", () => {
-            console.log("Plaay")
             chrome.runtime.sendMessage({ play: true }, function (response) {
                 console.log(response);
             });
         })
 
         video.addEventListener("pause", () => {
-            console.log("Pauuuse")
             chrome.runtime.sendMessage({ pause: true }, function (response) {
                 console.log(response);
             });
@@ -22,6 +18,7 @@
     }
 
     let user = document.getElementById("dropdown08")?.text?.replace("(Logout)", "")?.trim();
+
     chrome.runtime.sendMessage({ studentId: user }, function (response) {
         console.log(response);
     });
